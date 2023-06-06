a6b0f85f69eb08808e381eb36488bad104e5a3d6
claytonvk <clayvanderkolk@gmail.com>
2023-06-06 17:59:12 -0400

    Additional Mobile Capatability, Menu Additions

--- app.js
+++ app.js
@@ -21,3 +21,29 @@ function changeSlide(n) {
     slides[i].style.transform = 'translateX(' + translateX + 'px)';
   }
 }
+
+$(function() {
+  $("#datepicker").datepicker();
+});
+
+// Get the phone input element
+const phoneInput = document.getElementById('phone-input');
+phoneInput.addEventListener('input', formatPhoneNumber);
+
+function formatPhoneNumber() {
+  // Get the raw phone number value
+  let phoneNumber = phoneInput.value;
+
+  // Remove any non-digit characters
+  phoneNumber = phoneNumber.replace(/\D/g, '');
+
+  // Apply formatting based on the number of digits
+  if (phoneNumber.length > 3 && phoneNumber.length <= 6) {
+    phoneNumber = phoneNumber.replace(/(\d{3})(\d{1,3})/, '($1) $2');
+  } else if (phoneNumber.length > 6) {
+    phoneNumber = phoneNumber.replace(/(\d{3})(\d{3})(\d{1,4})/, '($1) $2-$3');
+  }
+
+  // Update the input value with the formatted phone number
+  phoneInput.value = phoneNumber;
+}
--- send_email.php
+++ send_email.php
@@ -2,16 +2,28 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $name = $_POST['name'];
   $email = $_POST['email'];
-  $message = $_POST['message'];
+  $phone = $_POST['phone'];
+  $instagram = $_POST['instagram'];
+  $heard = $_POST['heard'];
+  $package = $_POST['package'];
+  $who = $_POST['who'];
+  $date = $_POST['date'];
+  $etc = $_POST['etc'];
   
-  $to = 'your_email@example.com'; // Replace with your email address
-  $subject = 'New Contact Form Submission';
-  $body = "Name: $name\nEmail: $email\n\n$message";
+  $body = "Name: $name\nEmail: $email\nPhone #: $phone\nInstagram Handle: $instagram\n\nHow did you hear about me? $heard\nWhich package are you interested in? $package\nWho will be in the photos? $who\nDate: $date\nAnything else? $etc";
   
-  if (mail($to, $subject, $body)) {
-    echo 'Email sent successfully';
-  } else {
-    echo 'Error sending email';
-  }
+  echo "<pre>$body</pre>";
+
+  // $to = 'vkfilmandphotostudios@gmail.com'; // Replace with your email address
+  // $subject = 'New Contact Form Submission';
+  // $body = "Name: $name\nEmail: $email\nPhone #: $phone\nInstagram Handle: $instagram\n
+  // How did you hear about me? $heard\nWhich package are you interested in? $package
+  // \nWho will be in the photos? $who\nDate: $date\nAnything else? $etc";
+  
+//   if (mail($to, $subject, $body)) {
+//     echo 'Email sent successfully';
+//   } else {
+//     echo 'Error sending email';
+//   }
 }
 ?>
